3d012b0055de7a56528582447abe54433f770ef4
helioborges164 <helioborges1648@gmail.com>
2025-03-31 20:01:08 -0300

    Adicionado operador de comparacao nos valores min e max

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ function generateNumber() {
     const min = Math.ceil(document.getElementById("min-number").value)
     const max = Math.floor(document.getElementById("max-number").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min)
+    if (min >= max) {
+        alert("O valor mínimo não pode ser menor e nem igual ao valor máximo")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min)
 
     alert(result)
+    }
 }
 
 sortButton.addEventListener("click", generateNumber)
